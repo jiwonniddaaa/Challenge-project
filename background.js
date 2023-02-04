6a0283178bf1f06375a745d6dc5e170ec32c6051
jiwonniddaaa <120795436+jiwonniddaaa@users.noreply.github.com>
2023-02-04 14:15:58 +0900

    Create background.js

---
+++ background.js
@@ -0,0 +1,21 @@
+const images = [
+    "0.jpg",
+    "1.jpg",    
+    "2.jpg",
+    "3.jpg"
+]
+const locate = [
+    "Amalfi, Amalfi Coast",
+    "Paris, arc de triomphe",
+    "London, tower bridge",
+    "Rome, trevi fountain",
+]
+const random = Math.floor(Math.random() * images.length);
+const chosenImage = images[random];
+const locateElement = document.body.querySelector("div #location");
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+document.body.appendChild(bgImage);
+locateElement.innerText = locate[random];
