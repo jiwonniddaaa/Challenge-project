a657698da25104fd197f80912da02bb42877abcc
jiwonniddaaa <120795436+jiwonniddaaa@users.noreply.github.com>
2023-02-04 14:55:24 +0900

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -1,14 +1,12 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-// const link = document.querySelector("a");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginBtnClick(event){
     event.preventDefault(); 
-    // const username = loginInput;
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
@@ -17,17 +15,14 @@ function onLoginBtnClick(event){
 
 function paintGreetings(username){
     greeting.innerText = `Hello ${username}`;
-    // greeting.innerText="Hello  " + username;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null){
-    ///show the form 
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginBtnClick);
 }else{
-    ///show the greetings
    paintGreetings(savedUsername);
 }
